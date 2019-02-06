25ec3e449ea8e396fb31b1198be35d439386aa7e
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-02-06 11:58:24 -0500

    Allow for multiple branches to be deleted at once

--- windows/git/gitbranchdelete.bat
+++ windows/git/gitbranchdelete.bat
@@ -1,10 +1,20 @@
 @echo off
 if [%1]==[] goto :fail
-doforallgit git branch -D %1
+
+set argCount=0
+for %%x in (%*) do (
+  Set /A argCount+=1
+  if /I "%argCount%" NEQ "1" (
+    echo ---- Deleting branch: %%x ----
+    doforallgit git branch -D %%x
+  )
+) 
+
+
 goto :finished
 
 :fail
-echo Usage: gitbranchdelete <branchname>
+echo Usage: gitbranchdelete <branchnames>
 
 :finished
 echo Done
