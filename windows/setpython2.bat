287f8cdadf10f964735f67f737a7642ca2295b50
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-04-02 17:19:04 -0400

    Adding simple scripts for complex world of python

---
+++ windows/setpython2.bat
@@ -0,0 +1,3 @@
+set PATH=C:\Python27;C:\Python27\Scripts;C:\Python27\lib;C:\Python27\lib\site-packages;%PATH%
+set PYTHONPATH=C:\Python27\Lib;.
+set PYTHONHOME=C:\Python27
---
+++ windows/setpython3.bat
@@ -0,0 +1,3 @@
+set PATH=C:\Python37;C:\Python37\Scripts;C:\Python27\lib;C:\Python27\lib\site-packages;%PATH%
+set PYTHONPATH=C:\Python37\Lib;.
+set PYTHONHOME=C:\Python37
